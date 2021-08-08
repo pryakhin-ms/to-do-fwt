98083aa68870cffe209e7fdfdf65d05ad05772be
PryakhinMS <pryakhin.ms@gmail.com>
2021-08-08 13:32:27 +0400

    Улучшена вёрстка компонента Form. Отключён автокомплит.

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
@@ -6,28 +7,14 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiButton-root': {
-      width: '20%',
-      margin: theme.spacing(0.5),
+      width: '100px',
+      margin: theme.spacing(1),
       borderColor: theme.palette.success.light,
       color: theme.palette.success.light,
     },
     '& .MuiTextField-root': {
-      margin: theme.spacing(0.5),
-      width: '75%',
-      '& fieldset': {
-        borderColor: theme.palette.success.light,
-      },
-      '&:hover fieldset': {
-        borderColor: theme.palette.success.light,
-      },
-      '& .Mui-focused': {
-        color: theme.palette.success.light,
-        borderColor: theme.palette.success.light,
-      },
-      '& .Mui-focused fieldset': {
-        color: theme.palette.success.light,
-        borderColor: theme.palette.success.light,
-      },
+      width: '100%',
+      minWidth: '145px',
     },
   },
 }));
@@ -36,11 +23,15 @@ function Form(props) {
   const { addTask } = props;
   const classes = useStyles();
   return (
-    <form className={classes.root} onSubmit={addTask}>
-      <TextField size="small" id="outlined-basic" label="Введите задачу" variant="outlined" />
-      <Button variant="outlined" disabled={false} type="submit" size="medium">
-        ADD
-      </Button>
+    <form className={classes.root} onSubmit={addTask} autoComplete="off">
+      <Box display="flex" flexWrap="wrap">
+        <Box flexGrow={1} p={1}>
+          <TextField size="small" id="outlined-basic" label="Введите задачу" variant="outlined" />
+        </Box>
+        <Button variant="outlined" type="submit">
+          ADD
+        </Button>
+      </Box>
     </form>
   );
 }
